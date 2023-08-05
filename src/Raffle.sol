a81e9e6c4e3341ae83a7a1b8c42e1e2ee9fce0de
udeshika <perera.n.u@gmail.com>
2023-08-05 11:39:20 +0800

    Added winner picked function and integrated VRF

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,53 +2,104 @@
 pragma solidity ^0.8.20;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 /**
  * @title Sample Raffle contract
  * @author UdeRox
  * @notice This contract is created following Pratric's youtube tutorials.
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughETHSend();
+    error Raffle__TransferFailed();
+    error Raffle__NotOpened();
 
-    uint256 private immutable i_enteranceFee;
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+    uint32 private MIN_WORDS = 1;
+    uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint16 private immutable i_minBlockConfirmation;
+    uint64 private immutable i_subId;
+    uint32 private immutable i_callbackGasLimit;
+
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
 
     event EnteredRaffle(address indexed player);
+    event WinnerPicked(address indexed winner);
 
-    constructor(uint256 _enteranceFee, uint256 _interval) {
-        i_enteranceFee = _enteranceFee;
+    constructor(
+        uint256 _i_entranceFee,
+        uint256 _interval,
+        address _vrfCoordinator,
+        bytes32 _gasLane,
+        uint16 _minBlockConfirmation,
+        uint64 _subId,
+        uint32 _callbackGasLimit
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
+        i_entranceFee = _i_entranceFee;
         i_interval = _interval;
         s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
+        i_gasLane = _gasLane;
+        i_minBlockConfirmation = _minBlockConfirmation;
+        i_subId = _subId;
+        i_callbackGasLimit = _callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() external payable {
-        // require((msg.value >= 0.1 ether), "You need to send at least one 0.1ETH");
-        if (msg.value <= 0.1 ether) {
+        if (msg.value <= i_entranceFee) {
             revert Raffle__NotEnoughETHSend();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpened();
         }
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickAwinner() public {
+    function pickAwinner() external {
+        s_lastTimeStamp = block.timestamp;
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
-        s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subId,
+            i_minBlockConfirmation,
+            i_callbackGasLimit,
+            MIN_WORDS
+        );
+    }
 
-        // requestId = COORDINATOR.requestRandomWords(
-        //     keyHash,
-        //     s_subscriptionId,
-        //     requestConfirmations,
-        //     callbackGasLimit,
-        //     numWords
-        // );
+    function fulfillRandomWords(
+        uint256 /*requestId*/,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 winnerIndex = randomWords[0] % s_players.length;
+        address payable winner = s_players[winnerIndex];
+        s_recentWinner = winner;
+
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(winner);
     }
 
     function getEnteranceFee() external view returns (uint256) {
-        return i_enteranceFee;
+        return i_entranceFee;
     }
 }
